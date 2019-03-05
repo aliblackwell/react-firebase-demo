f75528fba33ccd3a717bfa531cb5ce7c2e3a66e5
Alasdair Blackwell <Alasdair@ada.ac.uk>
2019-03-05 12:11:58 +0000

    Basic Formik example

--- src/add-task/index.js
+++ src/add-task/index.js
@@ -1,9 +1,54 @@
 import React from 'react'
+import { Formik }  from 'formik'
 
 const AddTask = (props) => {
-    return(
+
+    const submitToFirebase = (formValues) => {
+        // add the fields to firebase
+        // redirect the user to the homepage
+        console.log(formValues)
+    }
+
+
+    return (
         <div className="AddTask">
             <h1>Add a Task</h1>
+            <h1>My Form</h1>
+            <Formik
+                initialValues={{ name: '', description: '' }}
+                onSubmit={(values, actions) => {
+                    setTimeout(() => {
+                        //alert(JSON.stringify(values, null, 2));
+                        submitToFirebase(values)
+                        //actions.setSubmitting(false);
+                    }, 1000);
+                }}
+                render={props => (
+                    <form onSubmit={props.handleSubmit}>
+                        <input
+                            type="text"
+                            onChange={props.handleChange}
+                            onBlur={props.handleBlur}
+                            value={props.values.name}
+                            placeholder="Name of task"
+                            name="name"
+                        />
+                        {props.errors.name && <div id="feedback">{props.errors.name}</div>}
+                        <input
+                            type="textarea"
+                            onChange={props.handleChange}
+                            onBlur={props.handleBlur}
+                            value={props.values.description}
+                            placeholder="What's the task involve?"
+                            name="description"
+                        />
+                        {props.errors.description && <div id="feedback">{props.errors.description}</div>}
+                        <button type="submit">Submit</button>
+                    </form>
+                )}
+            />
+
+
         </div>
     )
 }
