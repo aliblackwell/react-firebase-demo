7e9c30fce03d527587e883ac5be978d60616db18
Alasdair Blackwell <Alasdair@ada.ac.uk>
2019-03-05 11:58:56 +0000

    Basic routing and navigation

---
+++ src/add-task/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddTask = (props) => {
+    return(
+        <div className="AddTask">
+            <h1>Add a Task</h1>
+        </div>
+    )
+}
+
+export default AddTask
